6ccdde919f29ffb8eb0edd403074891cc5550aed
VladDCGit <rv_22n@hotmail.com>
2021-12-02 18:42:58 -0600

    Nueva prueba

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import MainPage from './main-page';
 //# import './assets/styles/styles.scss';
-//comentario
+
 
 
 ReactDOM.render(
-    <App />,
+    <MainPage />,
   document.getElementById('root')
 );
 
