e0b336d0978ff1b5f6c1bc39b8bda33f922361c7
VladDCGit <rv_22n@hotmail.com>
2021-12-02 18:58:36 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import MainPage from './main-page';
+//import MainPage from './main-page';
 //# import './assets/styles/styles.scss';
 
 
