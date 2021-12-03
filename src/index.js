cadcfeab360f5bbe06165f0b5289c950c921bbe0
VladDCGit <rv_22n@hotmail.com>
2021-12-02 18:46:04 -0600

    Revert "Nueva prueba"
    This reverts commit 6ccdde919f29ffb8eb0edd403074891cc5550aed.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import MainPage from './main-page';
 //# import './assets/styles/styles.scss';
-
+//comentario
 
 
 ReactDOM.render(
-    <MainPage />,
+    <App />,
   document.getElementById('root')
 );
 
